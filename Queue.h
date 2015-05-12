5ce73cd6fba2bfb53d23cbe31b2a35da9967f96f
ZYueYang <1720075616@qq.com>
2015-05-12 16:50:24 +0800

    queue.h

---
+++ Queue.h
@@ -0,0 +1,13 @@
+#ifndef Queue_h
+#define Queue_h
+class Queue
+{
+    public:
+         Queue();
+       void  push();
+       template <T> pop();    
+    private:
+        template<T> a;
+
+};
+#endif
